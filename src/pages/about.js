ec2861e33fc96313e76efa19ad14640e79736860
jinyang023 <ii0273757@gmail.com>
2021-08-13 09:46:00 +0900

    hi

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,7 +13,7 @@ const AboutPage = ({ location, data }) => (
       <div className="aboutstyle">
       <p><img alt="iPhone_01" src={imgB} width='600' height='400'/></p>
       <p>
-          안녕하세요<br/>
+          ㅎㅇ<br/>
           다양한 정보를 공유하는 블로그입니다.
       </p>
       <p>급변하는 시대에서 필요한 기술을 접목하여 새로운 가치를 창출할 수 있도록 노력합니다.</p>
